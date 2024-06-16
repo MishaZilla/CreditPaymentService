d947e58cb2d0ff8c747fedf065b0d202e4f23954
Mihail Test <misha.lucenko17@gmail.com>
2024-06-16 15:13:48 +0300

    Isparvlen commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public int calculate(int credit, double percent, int month) {
-        double k = (percent/100/12*(Math.pow(1+percent/100/12, month)))/((Math.pow(1+percent/100/12, month))-1);
-        return (int) (k*credit);
+        double k = (percent / 100 / 12 * (Math.pow(1 + percent / 100 / 12, month))) / ((Math.pow(1 + percent / 100 / 12, month)) - 1);
+        return (int) (k * credit);
     }
 }
